7bfd7bfc6588fd19e3233a88b624e7e94e17f816
DanieLovera <daanilo2497@gmail.com>
2022-01-21 13:16:48 -0300

    Docs: Add swagger for API doc

--- backend/src/main/java/com/exercise/backend/BackendApplication.java
+++ backend/src/main/java/com/exercise/backend/BackendApplication.java
@@ -2,12 +2,34 @@ package com.exercise.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BackendApplication {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
